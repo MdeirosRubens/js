74221c090fd3158b9962d1dd7c699c7ef63f8a44
MdeirosRubens <mdeirosrubens@outlook.com>
2022-04-26 16:52:21 -0300

    Criando contaSalario e aplicando metodo abstrato

--- Conta.js
+++ Conta.js
@@ -1,5 +1,9 @@
 export class Conta{
     constructor(saldoInicial, cliente, agencia) {
+        if(this.constructor == Conta){
+            throw new Error("Você não deveria instanciar um objeto do tipo conta, pois essa é uma classe abstrata")
+        }
+
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
@@ -19,9 +23,9 @@ export class Conta{
         return this._saldo;
     }
 
+    //metodo abstrato
     sacar(valor) {
-        let taxa =1
-        return this._sacar(valor, taxa);
+        throw new Error("O metodo sacar da conta é abstrato")
     }
 
     _sacar(valor, taxa){
